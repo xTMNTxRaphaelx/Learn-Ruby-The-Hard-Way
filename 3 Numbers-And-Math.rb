29cb054831ad434bdfacb26856435159046442fa
Rahul Garg <fructis_n3m0@live.in>
2014-07-02 08:19:26 +0530

    Commit-Ni
    Ohayoo Gozaimasu- 02072014
    
    Completed First Three Exercises of Learn Ruby the Hard Way

---
+++ 3 Numbers-And-Math.rb
@@ -0,0 +1,37 @@
+# This prints out the statement.
+puts "I will now count my chickens:"
+
+# This prints out the statement as well as calculate 25 + (30/6)
+puts "Hens", 25.0 + 30.0 / 6.0
+# This prints out the statement as well as calculate 100 - ((25*3)%4)
+puts "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# This prints out the statement.
+puts "Now I will count the eggs:"
+
+# This calculate 3 + 2 + 1 - 5 + (4%2) - (1/4) + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# This prints out the statement.
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# This prints out if 3 + 2 < 5 - 7 in boolean
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# This prints out the statement as well as calculate 3 + 2
+puts "What is 3 + 2?", 3.0 + 2.0
+# This prints out the statement as well as calculate 5 - 7
+puts 'What is 5 - 7?', 5.0 - 7.0
+
+# This prints out the statement.
+puts "Oh, that's why it's false."
+
+# This prints out the statement.
+puts "How about some more."
+
+# This prints out the statement as well as calculate if 5 > -2
+puts "Is it greater?", 5.0 > -2.0
+# This prints out the statement as well as calculate if 5 >= -2
+puts "Is it greater or equal?", 5.0 >= -2.0
+# This prints out the statement as well as calculate if 5 <= -2
+puts "Is it less or equal?", 5.0 <= -2.0
